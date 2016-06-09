d29645e058f583272647b44a0a7ae701325421ae
Lefort, Maxence C <maxence.lefort@nist.gov>
2016-06-09 14:51:25 -0400

    add stats logs

--- hit-core-edi-service/src/main/java/gov/nist/hit/core/service/edi/EDIMessageValidator.java
+++ hit-core-edi-service/src/main/java/gov/nist/hit/core/service/edi/EDIMessageValidator.java
@@ -11,10 +11,13 @@
  */
 package gov.nist.hit.core.service.edi;
 
+import gov.nist.healthcare.unified.exceptions.ConversionException;
+import gov.nist.healthcare.unified.exceptions.NotFoundException;
+import gov.nist.healthcare.unified.model.*;
+import gov.nist.healthcare.unified.model.Collection;
 import gov.nist.hit.core.edi.domain.EDITestContext;
 
 import gov.nist.healthcare.unified.enums.Context;
-import gov.nist.healthcare.unified.model.EnhancedReport;
 import gov.nist.healthcare.unified.proxy.ValidationProxy;
 import gov.nist.hit.core.domain.MessageValidationCommand;
 import gov.nist.hit.core.domain.MessageValidationResult;
@@ -29,14 +32,17 @@ import hl7.v2.validation.vs.ValueSetLibrary;
 import hl7.v2.validation.vs.ValueSetLibraryImpl;
 
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 
 public abstract class EDIMessageValidator implements MessageValidator {
+
+  private static Log statLog = LogFactory.getLog("StatLog");
 
   @Override
   public MessageValidationResult validate(TestContext testContext, MessageValidationCommand command)
@@ -44,15 +50,94 @@ public abstract class EDIMessageValidator implements MessageValidator {
     try {
       EnhancedReport report = generateReport(testContext, command);
       if (report != null) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Detections detections = report.getDetections();
+        boolean validationSuccess = true;
+        StringBuilder validationResult = new StringBuilder();
+
+        for (Classification classification : detections.classes()) {
+          if (classification.getName().equals("Affirmative")) {
+
+          } else if (classification.getName().equals("Warning")) {
+            int warningCount = classification.keys().size();
+            validationResult.append(" - ");
+            validationResult.append(warningCount);
+            validationResult.append(" warning");
+            if (warningCount > 1) {
+              validationResult.append("s");
+            }
+          } else if (classification.getName().equals("Error")) {
+            validationSuccess = false;
+            int errorCount = classification.keys().size();
+            if (errorCount > 0) {
+              HashMap<String, Integer> segmentCount = new HashMap<>();
+              validationResult.append(" - ");
+              validationResult.append(errorCount);
+              validationResult.append(" error");
+              if (errorCount > 1) {
+                validationResult.append("s");
+              }
+              for (String key : classification.keys()) {
+                Collection collection = classification.getArray(key);
+                collection.getName();
+                for (int i = 0; i < collection.size(); i++) {
+                  Section section = collection.getObject(i);
+                  String path = section.getString("path");
+                  if (path != null && !"".equals(path)) {
+                    path = path.split("\\[")[0];
+                    int segmentErrorCount = 1;
+                    if (segmentCount.containsKey(path)) {
+                      segmentErrorCount = segmentCount.get(path) + 1;
+                    }
+                    segmentCount.put(path, segmentErrorCount);
+                  }
+                }
+              }
+              boolean isFirst = true;
+              if(segmentCount.size()>0) {
+                validationResult.append(" [");
+                for (String path : segmentCount.keySet()) {
+                  if (!isFirst) {
+                    validationResult.append(", ");
+                  }
+                  validationResult.append(path);
+                  validationResult.append(" (");
+                  validationResult.append(segmentCount.get(path));
+                  validationResult.append(" error");
+                  if (segmentCount.get(path) > 1) {
+                    validationResult.append("s");
+                  }
+                  validationResult.append(")");
+                  isFirst = false;
+                }
+                validationResult.append("]");
+              }
+            }
+          }
+        }
+        StringBuilder validationLog = new StringBuilder();
+        validationLog.append(simpleDateFormat.format(new Date()));
+        validationLog.append(" [Validation] EDI - ");
+        EDITestContext ediTestContext = (EDITestContext)testContext;
+        if(ediTestContext.getType()!=null) {
+          validationLog.append(ediTestContext.getType());
+          validationLog.append(" - ");
+        }
+        if (validationSuccess)
+          validationLog.append("success");
+        else
+          validationLog.append("fail");
+        validationLog.append(validationResult.toString());
+        statLog.info(validationLog.toString());
         Map<String, String> nav = command.getNav();
         if (nav != null && !nav.isEmpty()) {
           report.setTestCase(nav.get("testPlan"), nav.get("testGroup"), nav.get("testCase"),
                   nav.get("testStep"));
         }
-        return new MessageValidationResult(report.to("json").toString(), report.render("iz-report",
-                null));
+        return new MessageValidationResult(report.to("json").toString(), report.render("iz-report", null));
       }
-      throw new MessageValidationException();
+
+    throw new MessageValidationException();
     } catch (MessageException e) {
       throw new MessageValidationException(e.getLocalizedMessage());
     } catch (RuntimeException e) {
